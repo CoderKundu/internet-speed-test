fef817f08c2ceaafbbfad0c049c6125edc75a52b
CoderKundu <prasoonk2810@gmail.com>
2026-02-03 19:50:03 +0530

    Prepare frontend for Netlify deployment

--- frontend/src/services/speedtest.api.js
+++ frontend/src/services/speedtest.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function fetchSpeedTest() {
-  const res = await axios.get("http://localhost:5000/api/speedtest");
+  const res = await axios.get("http://YOUR-BACKEND-URL/api/speedtest");
   return res.data;
 }
